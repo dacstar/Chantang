1af8ddc85ef4573778f610f20a3c9a3b9eaa9444
sanghonam <ska0294@google.com>
2020-11-10 19:23:08 +0900

    Apply validateDuplicateUser

--- back/src/main/java/com/example/back/service/MemberService.java
+++ back/src/main/java/com/example/back/service/MemberService.java
@@ -1,13 +1,15 @@
 package com.example.back.service;
 
+import com.example.back.dto.LoginRequest;
+import com.example.back.dto.LoginResponse;
 import com.example.back.entity.Member.MemberEntity;
 import com.example.back.repository.MemberRepository;
+import com.example.back.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.security.util.Password;
 
 import java.util.List;
 
@@ -23,18 +25,18 @@ public class MemberService {
     // save user
     @Transactional
     public Long join(MemberEntity user) {
+        validateDuplicateUser(user);
         String encodePassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodePassword);
         memberRepository.save(user);
-        validateDuplicateUser(user);
         return user.getId();
     }
 
     // 중복 회원 검사
     private void validateDuplicateUser(MemberEntity user) {
-        List<MemberEntity> findUsers = memberRepository.findByName(user.getName());
-        if (findUsers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다");
+        List<MemberEntity> findUser = memberRepository.findByMemberId(user.getMemberId());
+        if (findUser.isEmpty()) {
+            throw new IllegalStateException("exist_member");
         }
     }
 
