83a65b26f1f162a7f86797d136f6c8ca1fd090c3
sanghonam <ska0294@google.com>
2020-11-10 19:23:47 +0900

    Edit SecurityConfig File Name

--- back/src/main/java/com/example/back/config/SecurityConfig.java
+++ back/src/main/java/com/example/back/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
