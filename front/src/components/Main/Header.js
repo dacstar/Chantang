bfbe5742f46bfc19e29da73cb570b1a0800c7843
unknown <wkdus7h@naver.com>
2020-11-16 17:06:39 +0900

    change eslint setting

--- front/src/components/Main/Header.js
+++ front/src/components/Main/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
 import HomeIcon from '@material-ui/icons/Home';
 
 const useStyles = makeStyles((theme) => ({
